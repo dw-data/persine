426f1fb81bb1ea2baa8d940794803d1a15ae403a
RodrigoMenegat <rodrigoschuinski@gmail.com>
2021-09-15 19:49:38 -0300

    Fixes endless wait for ariaBusy

--- persine/bridges/amazon.py
+++ persine/bridges/amazon.py
@@ -39,10 +39,146 @@ class AmazonBridge(BaseBridge):
         """
         )  # noqa: E501
 
+
+    def __scrape_suggested_products(self):
+
+        return self.driver.execute_async_script(
+            """                
+            let onComplete = arguments[0];
+
+            function getItemDetails(d) {
+
+                let section = d.closest(".a-carousel-container")
+                let data = {}
+
+                try { data['title'] = d.querySelector('img')['alt']; } catch(err) {}
+                try { data['title'] = d.querySelector('.pba-lob-bundle-title .a-truncate-full').innerText; } catch(err) {}
+                try { data['title'] = d.querySelector('.p13n-sc-truncated').innerText; } catch(err) {}
+                try { data['asin'] = JSON.parse(d.querySelector("div.p13n-asin").dataset['p13nAsinMetadata'])['asin'] } catch(err) {}
+                try { data['url'] = d.querySelector('a.a-link-normal')['href']; } catch(err) {}
+                try { data['img'] = d.querySelector('img')['src']; } catch(err) {}
+                try { section.querySelector("h2 .sp_desktop_sponsored_label").remove() } catch(err) {}
+                try { section.querySelector("h2 a").remove() } catch(err) {}
+                try { data['section_title'] = section.querySelector("h2").innerText; } catch(err) {}
+                try { data['is_sponsored'] = section.innerText.indexOf("Sponsored") != -1 } catch(err) {}
+                try { data['stars'] = d.querySelector('[title*="out of 5 stars"]').title; } catch(err) {}
+                try { data['review_count'] = d.querySelector('[[title*="out of 5 stars"] .a-size-small').innerText; } catch(err) {}
+
+                if(!data['stars']) {
+                    try { data['stars'] = d.querySelector('.adReviewLink i').classList[2]; } catch(err) {}
+                }
+                if(!data['review_count']) {
+                    try { data['review_count'] = d.querySelector('.adReviewLink span').innerText; } catch(err) {}
+                }
+
+                try { data['best_seller'] = d.querySelector('.p13n-best-seller').innerText; } catch(err) {}
+                try { data['price'] = d.querySelector('.a-price .a-offscreen').innerText.trim(); } catch(err) {}
+                try { data['price'] = d.querySelector('.p13n-sc-price,.a-color-price').innerText.trim(); } catch(err) {}
+                try { data['free_shipping'] = d.innerText.indexOf("FREE shipping") != -1 } catch(err) {}
+                try { data['is_prime'] = d.querySelector('.a-icon-prime') != null; } catch(err) {}
+                return data;    
+            }
+
+            function getCarouselCurrentContents(root) {
+                return [...root.querySelectorAll(".a-carousel-card:not(.vse-video-card)")].map((d, i) => {
+                    let data = getItemDetails(d)
+                    data['index'] = i;
+                    return data; 
+                });
+            }
+
+            async function getContentsOfCarousel(root, previousFirst, step=0) {
+                if(step > 10) {
+                    return Promise.resolve([])
+                }
+                if(!root.offsetParent) {
+                    console.log(root.offsetParent)
+                    return Promise.resolve([])
+                }
+                return new Promise((resolve, reject) => {
+                    let contents = getCarouselCurrentContents(root)
+                    let firstVisible = parseFloat(root.querySelector(".a-carousel-firstvisibleitem").value)
+                    firstVisible = firstVisible == "" ? 1 : parseInt(firstVisible)
+                    if(firstVisible != 1 || step == 0) {
+                        console.log('scrolling to', root)
+                        root.scrollIntoView()
+                        root.querySelector(".a-carousel-goto-nextpage").click()
+
+                        let tries = 0;
+                        let waitUntilReady = setInterval(function() {
+                            //let isBusy = root.querySelector(".a-carousel").ariaBusy;
+                            let isLoaded = root.querySelectorAll(".a-carousel-card-empty").length == 0;
+                            //console.log("isBusy?", isBusy);
+                            console.log("isLoaded?", isLoaded);
+                            if (isLoaded) {
+                                console.log("scraping carrousel", root);
+                                clearInterval(waitUntilReady);
+                                getContentsOfCarousel(root, firstVisible, step + 1)
+                                    .then(nextPageContents => {
+                                        resolve([...contents, ...nextPageContents]);
+                                    })
+                            } 
+                            else if (tries > 10) {
+                                
+                                // Remove empty cards
+                                console.log('Removed unload elements after considerable wait');
+                                toRemove = root.querySelectorAll(".a-carousel-card-empty");
+
+                                for (let item of toRemove) {
+                                    item.remove();
+                                }
+                            }
+                            else {
+                                console.log("working");
+                                tries += 1;
+                            }
+                        }, 200);
+                    } else {
+                        resolve(contents)
+                    }
+                })
+            }
+
+            async function scrapePage() {
+                let results = [];
+                let buttons = [...document.querySelectorAll(".a-carousel-goto-nextpage")]
+                let roots = buttons.map(b => {
+                    let root = b.closest(".a-carousel-container")
+                    root.style.borderWidth = '10px'
+                    root.style.borderColor = 'magenta'
+                    root.style.borderStyle = 'solid'
+                    root.style.background = '#fff880'
+                    return root
+                })
+
+                for(let i=0; i < roots.length; i++) {
+
+                    let carouselOptions = JSON.parse(
+                        roots[i].getAttribute("data-a-carousel-options")
+                    );
+
+                    if (carouselOptions["name"] === "rich-product-information-carousel") {
+                        console.log("ignoring details carousel");
+                        continue;
+                    }
+
+                    results = results.concat(await getContentsOfCarousel(roots[i]));
+                    roots[i].style.background = '#c7ff80'
+                };
+
+                onComplete(results)
+            }
+
+            scrapePage()
+            """
+        )  # noqa: E501
+
+
     def __force_page_contents_load(self):
 
         # Scrolls over the entirety of the page,
         # revealing all possible carousels
+
         self.driver.execute_script(
             """
             window.scrollTo({
@@ -50,29 +186,31 @@ class AmazonBridge(BaseBridge):
                 left: 0,
                 behavior: 'smooth'
             });"""
+
+            # time.sleep(2)
+            # self.driver.execute_script(
+            #     """
+            #     window.scrollTo({
+            #         top: document.body.scrollHeight,
+            #         left: 0,
+            #         behavior: 'smooth'
+            #     });"""
+            # )
+            # time.sleep(1)
+            # self.driver.execute_script(
+            #     "document.querySelectorAll('.a-carousel-goto-nextpage').forEach(e => e.click())"
+            # )
+            # time.sleep(1)
+            # self.driver.execute_script(
+            #     "document.querySelectorAll('.a-carousel-goto-nextpage').forEach(e => e.click())"
+            # )
+            # time.sleep(1)
+            # self.driver.execute_script(
+            #     "document.querySelectorAll('.a-carousel-goto-nextpage').forEach(e => e.click())"
+            # )
+    # time.sleep(1)
         )
-        # time.sleep(2)
-        # self.driver.execute_script(
-        #     """
-        #     window.scrollTo({
-        #         top: document.body.scrollHeight,
-        #         left: 0,
-        #         behavior: 'smooth'
-        #     });"""
-        # )
-        # time.sleep(1)
-        # self.driver.execute_script(
-        #     "document.querySelectorAll('.a-carousel-goto-nextpage').forEach(e => e.click())"
-        # )
-        # time.sleep(1)
-        # self.driver.execute_script(
-        #     "document.querySelectorAll('.a-carousel-goto-nextpage').forEach(e => e.click())"
-        # )
-        # time.sleep(1)
-        # self.driver.execute_script(
-        #     "document.querySelectorAll('.a-carousel-goto-nextpage').forEach(e => e.click())"
-        # )
-        # time.sleep(1)
+
 
     def __scrape_product_info(self):
 
@@ -151,8 +289,9 @@ class AmazonBridge(BaseBridge):
 
                         # General ranking (if any):
                         string = parent.text.split(":")[1]
-                        match = re.search("#(\d+) in (.*) \(.*", string)
-                        position = match.group(1).strip()
+                        print(string)
+                        match = re.search("#([\d,]+) in (.*) \(.*", string)
+                        position = match.group(1).strip().replace(",", "")
                         category = match.group(2).strip()
 
                         value[category] = position
@@ -162,7 +301,7 @@ class AmazonBridge(BaseBridge):
 
                         for ranking in rankings:
 
-                            match = re.search("#(\d+) in (.*)", ranking.text)
+                            match = re.search("#([\d,]+) in (.*)", ranking.text)
                             position = match.group(1).strip()
                             category = match.group(2).strip()
 
@@ -200,111 +339,6 @@ class AmazonBridge(BaseBridge):
 
         return data
 
-    def __scrape_suggested_products(self):
-        return self.driver.execute_async_script(
-            """                
-            let onComplete = arguments[0];
-
-            function getItemDetails(d) {
-                let section = d.closest(".a-carousel-container")
-                let data = {}
-
-                try { data['title'] = d.querySelector('img')['alt']; } catch(err) {}
-                try { data['title'] = d.querySelector('.pba-lob-bundle-title .a-truncate-full').innerText; } catch(err) {}
-                try { data['title'] = d.querySelector('.p13n-sc-truncated').innerText; } catch(err) {}
-                try { data['asin'] = JSON.parse(d.querySelector("div.p13n-asin").dataset['p13nAsinMetadata'])['asin'] } catch(err) {}
-                try { data['url'] = d.querySelector('a.a-link-normal')['href']; } catch(err) {}
-                try { data['img'] = d.querySelector('img')['src']; } catch(err) {}
-                try { section.querySelector("h2 .sp_desktop_sponsored_label").remove() } catch(err) {}
-                try { section.querySelector("h2 a").remove() } catch(err) {}
-                try { data['section_title'] = section.querySelector("h2").innerText; } catch(err) {}
-                try { data['is_sponsored'] = section.innerText.indexOf("Sponsored") != -1 } catch(err) {}
-                try { data['stars'] = d.querySelector('[title*="out of 5 stars"]').title; } catch(err) {}
-                try { data['review_count'] = d.querySelector('[[title*="out of 5 stars"] .a-size-small').innerText; } catch(err) {}
-
-                if(!data['stars']) {
-                    try { data['stars'] = d.querySelector('.adReviewLink i').classList[2]; } catch(err) {}
-                }
-                if(!data['review_count']) {
-                    try { data['review_count'] = d.querySelector('.adReviewLink span').innerText; } catch(err) {}
-                }
-
-                try { data['best_seller'] = d.querySelector('.p13n-best-seller').innerText; } catch(err) {}
-                try { data['price'] = d.querySelector('.a-price .a-offscreen').innerText.trim(); } catch(err) {}
-                try { data['price'] = d.querySelector('.p13n-sc-price,.a-color-price').innerText.trim(); } catch(err) {}
-                try { data['free_shipping'] = d.innerText.indexOf("FREE shipping") != -1 } catch(err) {}
-                try { data['is_prime'] = d.querySelector('.a-icon-prime') != null; } catch(err) {}
-                return data;    
-            }
-
-            function getCarouselCurrentContents(root) {
-                return [...root.querySelectorAll(".a-carousel-card:not(.vse-video-card)")].map((d, i) => {
-                    let data = getItemDetails(d)
-                    data['index'] = i;
-                    return data; 
-                });
-            }
-
-            async function getContentsOfCarousel(root, previousFirst, step=0) {
-                if(step > 10) {
-                    return Promise.resolve([])
-                }
-                if(!root.offsetParent) {
-                    console.log(root.offsetParent)
-                    return Promise.resolve([])
-                }
-                return new Promise((resolve, reject) => {
-                    let contents = getCarouselCurrentContents(root)
-                    let firstVisible = parseFloat(root.querySelector(".a-carousel-firstvisibleitem").value)
-                    firstVisible = firstVisible == "" ? 1 : parseInt(firstVisible)
-                    if(firstVisible != 1 || step == 0) {
-                        console.log('scrolling to', root)
-                        root.scrollIntoView()
-                        root.querySelector(".a-carousel-goto-nextpage").click()
-
-                        let waitUntilReady = setInterval(function() {
-                            let isBusy = root.querySelector(".a-carousel").ariaBusy
-                            let isLoaded = root.querySelectorAll(".a-carousel-card-empty").length == 0
-                            console.log(isBusy)
-                            if(isBusy == 'true' || !isLoaded) {
-                                console.log("working")
-                            } else {
-                                clearInterval(waitUntilReady)
-                                getContentsOfCarousel(root, firstVisible, step + 1)
-                                    .then(nextPageContents => {
-                                        resolve([...contents, ...nextPageContents])
-                                    })
-                            }
-                        }, 200);
-                    } else {
-                        resolve(contents)
-                    }
-                })
-            }
-
-            async function scrapePage() {
-                let results = [];
-                let buttons = [...document.querySelectorAll(".a-carousel-goto-nextpage")]
-                let roots = buttons.map(b => {
-                    let root = b.closest(".a-carousel-container")
-                    root.style.borderWidth = '10px'
-                    root.style.borderColor = 'magenta'
-                    root.style.borderStyle = 'solid'
-                    root.style.background = '#fff880'
-                    return root
-                })
-
-                for(let i=0; i < roots.length; i++) {
-                    results = results.concat(await getContentsOfCarousel(roots[i]));
-                    roots[i].style.background = '#c7ff80'
-                };
-
-                onComplete(results)
-            }
-
-            scrapePage()
-            """
-        )  # noqa: E501
 
     def __scrape_raw_carousel_data(self):
         return self.driver.execute_script("""
@@ -312,6 +346,7 @@ class AmazonBridge(BaseBridge):
             return {...d.dataset}
         })
         """)
+
 
     def get_data(self):
         parsed = urlparse(self.driver.current_url)
@@ -332,11 +367,12 @@ class AmazonBridge(BaseBridge):
             return {
                 "page_type": "product",
                 "product_info": self.__scrape_product_info(),
-                #"recommendations": self.__scrape_suggested_products(),
+                "recommendations": self.__scrape_suggested_products(),
                 #"carousels": self.__scrape_raw_carousel_data()
             }
 
     def run(self, url):
+
         parsed = urlparse(url)
 
         if parsed.scheme in ["http", "https"]:
